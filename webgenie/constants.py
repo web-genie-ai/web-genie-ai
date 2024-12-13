7c90dd49cc783b39f4608280818134814db20468
pycorn0729 <hayesdominique0729@gmail.com>
2024-12-13 06:04:26 -0600

    docs: added comments in constants

--- webgenie/constants.py
+++ webgenie/constants.py
@@ -1,5 +1,14 @@
+# backend api hotkey
 API_HOTKEY = "5D72esHuc1DxD6PD8S6VyU24bTHGQjHHyodzTGsem1sejUYj"
+
+# max synthetic history size
 MAX_SYNTHETIC_HISTORY_SIZE = 10
+
+# place holder image url
 PLACE_HOLDER_IMAGE_URL = "https://picsum.photos/seed/picsum/800/600"
+
+# screenshot script path
 SCREENSHOT_SCRIPT_PATH = "webgenie/rewards/metrics/screenshot_single.py"
+
+# work dir
 WORK_DIR = "work"
