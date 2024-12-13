45b3dd05264271d0876a55c22c80fe7e01930672
pycorn0729 <hayesdominique0729@gmail.com>
2024-12-13 10:49:11 -0600

    removed run scripts

--- run_miner.sh
+++
@@ -1,4 +0,0 @@
-export PYTHONPATH=.
-#--axon.port 5555
-python neurons/miners/miner.py --netuid 214 --subtensor.network test --wallet.name s-miner --wallet.hotkey miner1 --logging.debug --axon.port 8090
-
--- run_validator.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONASYNCIODEBUG=1
-export PYTHONPATH=.
-python neurons/validators/validator.py --netuid 214 --subtensor.network test --wallet.name sc-val1 --wallet.hotkey sh-val1 --logging.debug  --neuron.axon_port 8091
