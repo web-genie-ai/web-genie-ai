6313c10593c0b658a5364b04c82b7a3c3152789a
pycorn0729 <hayesdominique0729@gmail.com>
2024-12-13 09:38:56 -0600

    chore: added seperate_html_css

--- webgenie/helpers/htmls.py
+++ webgenie/helpers/htmls.py
@@ -11,6 +11,25 @@ from webgenie.constants import (
 )
 from webgenie.helpers.images import image_to_base64
 
+def seperate_html_css(html_content: str): 
+    soup = BeautifulSoup(html_content, 'lxml')
+
+    css = ''
+    for style_tag in soup.find_all('style'):
+        css += style_tag.get_text()
+    for style_tag in soup.find_all('style'):
+        style_tag.decompose()
+
+    head = soup.head
+    if not head:
+        head = soup.new_tag('head')
+        soup.html.insert(0, head)
+
+    link_tag = soup.new_tag('link', rel='stylesheet', href='styles.css')
+    head.append(link_tag)
+    cleaned_html = str(soup)
+    return cleaned_html, css
+
 def html_to_screenshot(html: str) -> str:
     html_path = f"{WORK_DIR}/screenshot_{uuid.uuid4()}.html"
     with open(html_path, "w") as f:
