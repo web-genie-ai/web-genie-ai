5a2d2cfd0c91cf134de79fcea62ad5c23c74e086
pycorn0729 <hayesdominique0729@gmail.com>
2024-12-14 05:55:10 -0600

    feat: added synthetic dataset

---
+++ webgenie/datasets/synthetic_dataset.py
@@ -0,0 +1,64 @@
+# The paper [Unlocking the conversion of Web Screenshots into HTML Code with the WebSight Dataset](https://arxiv.org/pdf/2403.09029v1#bib.bib5) is our inspiration.
+# We should use Mistral-7B-Instruct to generate concepts and use Deepseek-Coder-33b-instruct to generate html, but now we are using openai models here.
+
+import bittensor as bt
+import os
+from typing import List
+
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+from webgenie.datasets.dataset import Dataset, DatasetEntry
+from webgenie.prompts import PROMPT_GEN_CONCEPT, PROMPT_GEN_HTML
+
+class ConceptResponse(BaseModel):
+    concepts: List[str] = Field(description="The concept of the website")
+
+class HTMLResponse(BaseModel):
+    html: str = Field(description="The html code of the website")
+
+class SyntheticDataset(Dataset):
+    def __init__(self):
+        self.model = ChatOpenAI(
+            api_key= os.getenv("OPENAI_API_KEY"),
+            model_name="gpt-4o",
+            temperature=0.6,
+        )
+
+        self.concept_parser = JsonOutputParser(pydantic_object=ConceptResponse)
+        self.html_parser = JsonOutputParser(pydantic_object=HTMLResponse)
+
+    async def _generate_concepts(self):
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", PROMPT_GEN_CONCEPT),
+        ]) 
+        chain = prompt | self.model | self.concept_parser
+        response = chain.invoke({
+            "instructions": self.concept_parser.get_format_instructions()
+        })
+        return response["concepts"]
+
+    async def _generate_html(self, concept: str):
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", PROMPT_GEN_HTML),
+        ])
+        chain = prompt | self.model | self.html_parser
+        response = chain.invoke({
+            "concept": concept, 
+            "instructions": self.html_parser.get_format_instructions()
+        })
+        return response["html"]
+        
+    async def generate_context(self)->DatasetEntry:
+        if len(self.concepts) == 0:
+            self.concepts = await self._generate_concepts()
+        
+        concept = self.concepts.pop(0)
+        ground_truth_html = await self._generate_html(concept)
+        return DatasetEntry(
+            src="synthetic",
+            prompt=concept,
+            ground_truth_html=ground_truth_html,
+        )
