84182ab7d00b05dbbabd3ab5854ec4f0cdeeb1a4
donbusha <dondraybb@gmail.com>
2024-12-19 12:21:40 +0000

    feat: checked gpu

--- neurons/miners/hf_miner.py
+++ neurons/miners/hf_miner.py
@@ -5,16 +5,30 @@ from webgenie.base.neuron import BaseNeuron
 from webgenie.protocol import WebgenieTextSynapse, WebgenieImageSynapse
 from webgenie.helpers.images import base64_to_image
 
-from neurons.miners.hf_models.falcon7b import generate_html_from_text
+from webgenie.utils.gpus import get_gpu_info
+total_memory_mb, _, _ = get_gpu_info()
+
+if total_memory_mb is None:
+    raise ValueError("No GPU detected. HfMiner requires a GPU.")
+
+if total_memory_mb < 1024 * 25:
+    raise ValueError("Insufficient GPU memory. HfMiner requires at least 25GB of GPU memory.")
+
 from neurons.miners.hf_models.websight_finetuned import generate_html_from_image
+
+if total_memory_mb > 1024 * 50:
+    from neurons.miners.hf_models.falcon7b import generate_html_from_text
 
 class HfMiner:
     def __init__(self, neuron: BaseNeuron):
         self.neuron = neuron
-
+ 
     async def forward_text(self, synapse: WebgenieTextSynapse) -> WebgenieTextSynapse:  
         try:  
-            synapse.html = generate_html_from_text(synapse.prompt)
+            if total_memory_mb > 1024 * 50:
+                synapse.html = generate_html_from_text(synapse.prompt)
+            else:
+                synapse.html = "you don't have enough memory to generate html from text"
             return synapse
         except Exception as e:
             bt.logging.error(f"Error in HfMiner forward_text: {e}")
