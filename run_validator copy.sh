531387eb84aebba405aedca62ee1ae9e1740fbe8
pycorn0729 <hayesdominique0729@gmail.com>
2025-01-05 05:32:28 -0600

    chore: removed temp files

--- run_validator copy.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=.
-
-pm2 start neurons/validators/validator.py --name "webgenie_validator" --interpreter python -- --netuid 214 --subtensor.network test --wallet.name sc-val1 --wallet.hotkey sh-val1 --logging.debug --neuron.axon_port 8091
