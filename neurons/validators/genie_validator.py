fix: fix overflow

neurons/validators/genie_validator.py
@@ -276,10 +276,9 @@ class GenieValidator:
         if session not in hash_cache:
             session_start_block = session * SESSION_WINDOW_BLOCKS
             subtensor = self.neuron.subtensor
-            hash_cache[session] = int(
-                subtensor.get_block_hash(session_start_block),
-                16
-            )
+            block_hash = subtensor.get_block_hash(session_start_block)
+            # Take last 16 digits to avoid integer overflow
+            hash_cache[session] = int(block_hash[-16:], 16)
         return (hash_cache[session] + task_index) % np.iinfo(np.int64).max
 
     async def forward(self):