36576b9457c3f534c3d2cd5543ac7f3dcee21a06
pycorn0729 <hayesdominique0729@gmail.com>
2025-03-21 14:24:31 -0700

    chore: unuse ddos

--- webgenie/constants.py
+++ webgenie/constants.py
@@ -3,7 +3,7 @@ import os
 import psutil
 # Change this value when updating your code base.
 # Define the version of the webgenie.
-__VERSION__ = "1.3.1" # version
+__VERSION__ = "1.3.2" # version
 
 SPEC_VERSION = (
     (1000 * int(__VERSION__.split(".")[0]))
